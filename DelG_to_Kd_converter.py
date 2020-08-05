fbff4caa0e417acbc2432e08b907c2e48dbec3e0
Tjmcawesome <32533387+Tjmcawesome@users.noreply.github.com>
2020-08-05 11:54:19 +0200

    Update DelG_to_Kd_converter.py

--- DelG_to_Kd_converter.py
+++ DelG_to_Kd_converter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import math # This will import math module 
+import math # This will import math module
 
 delg = float(input("What is your DelG in kcal/mol?" ))
 Kd_value = math.exp((delg*1000)/(1.98*298.15)) # this will calculate the dissociation constant from the Gibbs free energy of binding
@@ -9,7 +9,10 @@ Kd_value2 = (math.exp((delg*1000)/(1.98*298.15)))*1000000000 # this will calcula
 print("Kd =", "{:e}".format(Kd_value), "M") # this will print out the results in M
 print("Kd =", "{:e}".format(Kd_value1), "microM") # this will print out the results in microM
 print("Kd =", "{:e}".format(Kd_value2), "nM") # this will print out the results in nM
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
-# to run the script use this command: python DelG_to_Kd_converter.py
+# to run the script in terminal use this command: python DelG_to_Kd_converter.py
 # if you are using this script, please cite: Shityakov, S.; Broscheit, J.; Forster, C., alpha-Cyclodextrin dimer complexes of dopamine and levodopa derivatives to assess drug delivery to the central nervous system: ADME and molecular docking studies. Int J Nanomedicine 2012, 7, 3211-9.
